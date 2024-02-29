8da7e3b39267d68f6c5c9fd1d1574848890155db
Sylvia Wang <sylviamiaowang@gmail.com>
2024-02-29 18:44:35 +1100

    setting up

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+       <div className="bg-theme-black container relative m-auto p-3 flex justify-between items-center">
+        <h1>what the fuck</h1>
+       </div>
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+
+    colors : {
+      "blue": "#461fbc",
+      "purple": "#211462",
+      "silver":"#e3e3dd",
+      "black":"#090909",
+      "pink":"#c9aec0"
+    }
+    
+  },
+  plugins: [],
+}
+
